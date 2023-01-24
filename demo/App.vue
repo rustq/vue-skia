c0779d3d03dbb07f6cce5a9b62aa619d7992f1b0
meloalright <meloalright@gmail.com>
2023-01-24 11:10:04 +0800

    Modified demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layer x="0" y="0" id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
     <v-circle x="120" y="220" />
   </v-layer>
 </template>
@@ -15,14 +15,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
