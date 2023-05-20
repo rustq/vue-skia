c74914b8ea9435430c44bf97c3765b267e54dc41
meloalright <meloalright@gmail.com>
2023-05-21 00:36:53 +0800

    make hello world demo correct and safe

--- vue-playground/src/components/HelloWorld.vue
+++ vue-playground/src/components/HelloWorld.vue
@@ -6,33 +6,37 @@
     </h2>
     <p v-if="loading">wasm loading</p>
     <v-surface v-if="!loading" :width="600" :height="400">
-      <template
-        :key="70 + index"
-        v-for="(_, index) in new Array(count).fill(true)"
-      >
-        <v-rect
-          :x="(index % 7) * 10 + 100"
-          :y="(index % 3) * 10 + 100"
-          :width="200"
-          :height="200"
-          :a="100"
-          :r="index % 5 ? 0 : 200"
-          :g="255"
-          :b="0"
-        />
+      <template v-if="count >= 0">
+        <template
+          :key="70 + index"
+          v-for="(_, index) in new Array(count).fill(true)"
+        >
+          <v-rect
+            :x="(index % 7) * 10 + 100"
+            :y="(index % 3) * 10 + 100"
+            :width="200"
+            :height="200"
+            :a="100"
+            :r="index % 5 ? 0 : 200"
+            :g="255"
+            :b="0"
+          />
+        </template>
       </template>
-      <template
-        :key="100 + index"
-        v-for="(_, index) in new Array(10 - count).fill(true)"
-      >
-        <v-circle
-          :cx="(index % 7) * 10 + 300"
-          :cy="200"
-          :r="10 + ((count === 8 && true) ? 30 : 0)"
-          :a="100"
-          :g="50"
-          :b="200"
-        />
+      <template v-if="10 - count >= 0">
+        <template
+          :key="100 + index"
+          v-for="(_, index) in new Array(10 - count).fill(true)"
+        >
+          <v-circle
+            :cx="(index % 7) * 10 + 300"
+            :cy="200"
+            :r="10 + (count >= 7 && true ? 30 : index)"
+            :a="100"
+            :g="50"
+            :b="200"
+          />
+        </template>
       </template>
     </v-surface>
   </div>
