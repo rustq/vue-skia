8b10eaad4d3d66bbf790651a86577ef6bae9ccd4
meloalright <meloalright@gmail.com>
2023-06-18 00:06:57 +0800

    0.0.2

--- vue-playground/src/code.ts
+++ vue-playground/src/code.ts
@@ -1,6 +1,6 @@
 export default `<v-surface :width="360" :height="360">
   <v-rect :x="10" :y="220" :width="30" :height="30" color="cyan" :style="'fill'" />
-  <v-line style="fill" :strokeWidth="8" color="black" :p1="[100, 260]" :p2="[50, 285]" />
+  <v-line :strokeWidth="8" color="black" :p1="[100, 260]" :p2="[50, 285]" />
   <v-round-rect
     :x="220" :y="50" :width="80" :height="80" :r="10" color="fuchsia" :style="'stroke'" />
   <v-circle :cx="200" :cy="260" :r="50" :style="'stroke'" color="fuchsia" />
