c2d53395abbd949d819f153265b73e176d443754
MeloAlright <meloalright@gmail.com>
2023-08-16 00:41:39 +0800

    0.0.4 (#8)

--- vue-playground/src/code.ts
+++ vue-playground/src/code.ts
@@ -1,9 +1,7 @@
 export default `<v-surface :width="360" :height="360">
   <v-rect :x="10" :y="220" :width="30" :height="30" color="cyan" :style="'fill'" />
   <v-line :strokeWidth="8" color="black" :p1="[100, 260]" :p2="[50, 285]" />
-  <v-round-rect
-    :x="220" :y="50" :width="80" :height="80" :r="10" color="fuchsia" :style="'stroke'" />
-  <v-circle :cx="200" :cy="260" :r="50" :style="'stroke'" color="fuchsia" />
+  <v-round-rect :x="220" :y="50" :width="80" :height="80" :r="10" color="fuchsia" :style="'stroke'" />
   <v-points :points="[
     [138, 10],
     [178, 90],
@@ -17,5 +15,12 @@ export default `<v-surface :width="360" :height="360">
     [98, 90],
     [138, 10],
   ]" :style="'fill'" :strokeWidth="1" :color="'rgba(200, 255, 0, 0.7)'" />
+  <v-group :x="200" :y="160" color="violet" :style="'stroke'" :invertClip="true">
+    <template #clip>
+      <v-circle :cx="8" :cy="68" :r="40" />
+    </template>
+    <v-circle :cx="0" :cy="60" :r="50" :style="'fill'" />
+    <v-circle :cx="0" :cy="60" :r="70" />
+  </v-group>
 </v-surface>
 `
