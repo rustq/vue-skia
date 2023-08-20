89908af18b2e2dab604f0e05eecbd54c06260cac
MeloAlright <meloalright@gmail.com>
2023-08-20 15:48:12 +0800

    chore: Recover the ssw.default() (#12)

--- vue-skia-framework/launch.ts
+++ vue-skia-framework/launch.ts
@@ -9,11 +9,13 @@ const launch = function () {
             SSWInitialHelper.initialStatus = 1;
             const wasm = import("soft-skia-wasm/soft_skia_wasm.js");
             wasm.then((ssw) => {
-                window.ssw = ssw;
-                while (SSWInitialHelper.initialSucceedCallbackQueue.length) {
-                    SSWInitialHelper.initialSucceedCallbackQueue.pop()();
-                }
-                resolve(void 0)
+                ssw.default().then(() => {
+                    global.ssw = ssw;
+                    while (SSWInitialHelper.initialSucceedCallbackQueue.length) {
+                        SSWInitialHelper.initialSucceedCallbackQueue.pop()();
+                    }
+                    resolve(void 0)
+                })
             })
         } else if (SSWInitialHelper.initialStatus === 1) {
             SSWInitialHelper.initialSucceedCallbackQueue.push(() => resolve(void 0));
