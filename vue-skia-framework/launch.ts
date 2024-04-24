41d3d0b68be82444b006404d4f882ef00915a019
meloalright <meloalright@gmail.com>
2024-04-25 01:53:10 +0800

    feat: update launch func of vue skia framework

--- vue-skia-framework/launch.ts
+++ vue-skia-framework/launch.ts
@@ -8,12 +8,13 @@ const launch = function () {
         if (SSWInitialHelper.initialStatus === 0) {
             SSWInitialHelper.initialStatus = 1;
             const wasm = import("soft-skia-wasm/soft_skia_wasm.js");
-            wasm.then((ssw) => {
-                    window.ssw = ssw;
-                    while (SSWInitialHelper.initialSucceedCallbackQueue.length) {
-                        SSWInitialHelper.initialSucceedCallbackQueue.pop()();
-                    }
-                    resolve(void 0)
+            wasm.then(async (ssw) => {
+                await ssw.default();
+                window.ssw = ssw;
+                while (SSWInitialHelper.initialSucceedCallbackQueue.length) {
+                    SSWInitialHelper.initialSucceedCallbackQueue.pop()();
+                }
+                resolve(void 0)
             })
         } else if (SSWInitialHelper.initialStatus === 1) {
             SSWInitialHelper.initialSucceedCallbackQueue.push(() => resolve(void 0));
