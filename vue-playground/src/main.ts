fb6c7b8b37a4431eed495e78f3ee96c62844fd19
MeloAlright <meloalright@gmail.com>
2024-08-18 11:50:48 +0800

    0.1.2 (#44)
    * Live demo (#42)
    
    * feat: add progress in vue playground
    
    * feat: add 600 delay
    
    * Attached (#43)
    
    * feat: add progress in vue playground
    
    * feat: add 600 delay
    
    * feat: check attached and isMounted
    
    * chore: playground ci package update
    
    * chore: playground try 0.1.2-beta vue-skia
    
    * version: 0.1.2

--- vue-playground/src/main.ts
+++ vue-playground/src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { VueSkia } from 'vue-skia'
+import Vue3Progress from "vue3-progress";
 
 const app = createApp(App);
 app.use(VueSkia);
+app.use(Vue3Progress, {
+    position: "fixed",
+    color: "rgb(0, 161, 132)",
+  })
 app.mount("#app");
